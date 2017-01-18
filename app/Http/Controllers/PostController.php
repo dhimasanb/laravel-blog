c9284819792668e0b4298016b24876cd618f7452
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-18 16:34:14 +0700

    tambah fungsi insert data dan validasi server

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -34,7 +35,21 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the data
+        $this->validate($request, array(
+                'title'         => 'required|max:255',
+                'body'          => 'required'
+            ));
+
+        // store in the database
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
