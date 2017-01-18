fc308c563ff029de7d6850c7adc8f86190128d06
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-18 20:40:07 +0700

    menggunaka eloquent untuk membaca data di database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -63,7 +63,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::find($id);
+        return view('posts.show')->withPost($post);
     }
 
     /**
